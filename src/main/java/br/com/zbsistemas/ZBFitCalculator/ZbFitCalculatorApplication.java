ad92dd94472bf382d1682f571f3c5fea7a5fddee
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2024-05-11 22:27:39 +0200

    springboot started

---
+++ src/main/java/br/com/zbsistemas/ZBFitCalculator/ZbFitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zbsistemas.ZBFitCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbFitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbFitCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zbsistemas/ZBFitCalculator/ZbFitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zbsistemas.ZBFitCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbFitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
