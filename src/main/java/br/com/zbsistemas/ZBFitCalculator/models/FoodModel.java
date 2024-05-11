6d9a36f0a47c1d1a42494b67915bfdcc02fe7161
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2024-05-11 23:12:29 +0200

    created FoodRepository

--- src/main/java/br/com/zbsistemas/ZBFitCalculator/models/FoodModel.java
+++ src/main/java/br/com/zbsistemas/ZBFitCalculator/models/FoodModel.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter @Setter
 @Entity
 @Table(name = "tb_food")
-public class FoodModel {
+public class FoodModel implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/br/com/zbsistemas/ZBFitCalculator/repositories/FoodRepository.java
@@ -0,0 +1,11 @@
+package br.com.zbsistemas.ZBFitCalculator.repositories;
+
+import br.com.zbsistemas.ZBFitCalculator.models.FoodModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface FoodRepository extends JpaRepository<FoodModel, UUID> {
+}
